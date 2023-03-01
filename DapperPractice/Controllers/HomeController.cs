a30f131c8c8a6f7d0a985d9cd57d14bc38ea870a
Shovon Raul <shovon.r@keka.com>
2023-03-01 18:17:46 +0530

    Add project files.

---
+++ DapperPractice/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Dapper;
+using DapperPractice.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace DapperPractice.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string ConnectionString = "Server=sqldev;DataBase=sqldb-kekabiz-analytics;User Id=dev;Password=E0xl8T0RoqPs$vE3;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true;Integrated Security=false;";
+
+        [HttpGet("GetBillingData")]
+        public async Task<IActionResult> Index()
+        {
+            var sql = @"SELECT
+                       [Name]
+                      ,[BillingStartDate]
+                      ,[IndustryType]
+                      ,[CustomerSuccessManager]
+                       FROM[sqldb-kekabiz-analytics].[dbo].[BillingAccount]
+                       WHERE Name = @name"
+                    ;
+
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var bill = await connection.QueryAsync<Billing>(sql,new {name = "Trinath" });
+                return Ok(bill);
+            }
+            
+        }
+    }
+}
---
+++ DapperPractice/Model/Billing.cs
@@ -0,0 +1,20 @@
+﻿namespace DapperPractice.Model
+{
+    public class Billing
+    {
+        /*
+         SELECT TOP (1000) [Id]
+      ,[Name]
+      ,[BillingStartDate]
+      ,[IndustryType]
+      ,[CustomerSuccessManager]
+  FROM [sqldb-kekabiz-analytics].[dbo].[BillingAccount]
+        */
+
+        public string Name { get; set; }
+        public DateTime BillingStartDate { get; set; }
+        public string IndustryType { get; set; }
+        public string CustomerSuccessManager { get; set; }
+
+    }
+}
